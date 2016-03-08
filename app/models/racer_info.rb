b6bf54642a806ade989b5b4c20a51cab8ad3ebc0
Cody Deckard <cjdeckard@gmail.com>
2016-03-08 20:53:04 +0000

    Racers Spec RQ04

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -9,4 +9,9 @@ class RacerInfo
   field :_id, default:->{racer_id}
   
   embedded_in :parent, polymorphic: true
+  
+  validates :first_name, :last_name, :birth_year, :gender, presence: true 
+  validates :gender, inclusion: { in: %w(M F), message: "must be M or F" }
+  validates :birth_year, numericality: {less_than: Date.current.year}
+
 end
