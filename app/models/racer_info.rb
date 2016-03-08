ffed8f8714e50cc5a2471f6d1fc7c2c43dcdb353
Cody Deckard <cjdeckard@gmail.com>
2016-03-08 02:03:06 +0000

    Racers Spec RQ01

---
+++ app/models/racer_info.rb
@@ -0,0 +1,10 @@
+class RacerInfo
+  include Mongoid::Document
+  field :fn, type: String, :as => :first_name
+  field :ln, type: String, :as => :last_name
+  field :g, type: String, :as => :gender
+  field :yr, type: Integer, :as => :birth_year
+  field :res, type: Address, :as => :residence
+  field :racer_id, :as => :_id
+  field :_id, default:->{racer_id}
+end
