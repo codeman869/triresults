37ce7aa3faa28bdc844b56515c933d3ce16e909a
Cody Deckard <cjdeckard@gmail.com>
2016-03-08 05:56:37 +0000

    Racer Spec RQ03

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,9 @@
 class Racer
   include Mongoid::Document
+  
+  embeds_one :info, autobuild: true, as: :parent, class_name: "RacerInfo"
+  
+  before_create do |racer|
+     racer.info.id = racer.id 
+  end
 end
