22aa54cea351b6b43d3e4f17cc6e692b40d9c75b
Cody Deckard <cjdeckard@gmail.com>
2016-03-09 14:33:13 +0000

    Races Spec RQ01

---
+++ app/models/race.rb
@@ -0,0 +1,11 @@
+class Race
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :n, type: String, as: :name
+  field :date, type: Date
+  field :loc, type: Address, as: :location
+  
+  
+  
+end
