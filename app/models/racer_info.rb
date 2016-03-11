9a7ac431da8f52ff8e44b9508badb48140ae752b
Cody Deckard <cjdeckard@gmail.com>
2016-03-11 22:34:02 +0000

    Flatten Attributes Spec RQ01

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -14,4 +14,16 @@ class RacerInfo
   validates :gender, inclusion: { in: %w(M F), message: "must be M or F" }
   validates :birth_year, numericality: {less_than: Date.current.year}
 
+  ["city", "state"].each do |action|
+    define_method("#{action}") do
+      self.residence ? self.residence.send("#{action}") : nil
+    end
+    define_method("#{action}=") do |name|
+      object=self.residence ||=Address.new
+      object.send("#{action}=",name)
+      self.residence=object
+    end
+  end
+  
+  
 end
