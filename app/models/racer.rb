d1970f951e34a71084aea53b0fc26a0875eacb32
Cody Deckard <cjdeckard@gmail.com>
2016-03-11 20:20:57 +0000

    Racer Results Spec RQ02

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -3,6 +3,8 @@ class Racer
   
   embeds_one :info, autobuild: true, as: :parent, class_name: "RacerInfo"
   
+  has_many "races", :class_name => "Entrant", foreign_key: "racer.racer_id", dependent:  :nullify, order: :"race.date".desc
+  
   before_create do |racer|
      racer.info.id = racer.id 
   end
