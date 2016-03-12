c348e14d2009b5f551255e0fbae64f95caad21c8
Cody Deckard <cjdeckard@gmail.com>
2016-03-12 05:29:29 +0000

    Service Facade Spec RQ03

--- app/models/race.rb
+++ app/models/race.rb
@@ -99,6 +99,24 @@ class Race
 
     end
 
-end
+  end
+
+  def create_entrant racer
+    entrant = Entrant.new
+    entrant.race = self.attributes.symbolize_keys.slice(:_id, :n, :date)
+    entrant.racer = racer.info.attributes
+    entrant.group = get_group(racer)
+    
+    self.events.each do |event|
+      entrant.send("#{event.name}=",event)
+    end
+    
+    if entrant.validate
+      entrant.bib = next_bib
+      entrant.save!
+    end
+    return entrant
+  end
+
 
 end
