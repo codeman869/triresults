c09bcd807ca3804335b9e655db4322ce3a1531cc
Cody Deckard <cjdeckard@gmail.com>
2016-04-08 19:45:12 +0000

    URI Spec RQ02

---
+++ app/controllers/api/races_controller.rb
@@ -0,0 +1,43 @@
+module Api
+   class RacesController < ApplicationController
+       def index
+       
+       end
+       
+       def show
+           
+       end
+   
+   end
+   
+    class RacersController < ApplicationController
+        def index
+            
+        end
+        
+        def show
+            
+        end
+    end
+    
+    class ResultsController < ApplicationController
+        def index
+            
+        end
+        
+        def show
+            
+        end
+    end
+    
+    class EntriesController < ApplicationController
+        def index
+            
+        end
+        
+        def show
+            
+        end
+    end
+    
+end
